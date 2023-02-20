9b3ea88002d620635bd4794496da18652a7916be
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-02-20 18:37:54 -0500

    second commit

--- react-components-props-demo/src/App.js
+++ react-components-props-demo/src/App.js
@@ -1,8 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar.js';
-import Greeting from './components/Greeting.js';
+import Navbar from './components/Navbar';
+import Greeting from './components/Greeting';
 import StudentCard from './components/Studentcard';
+
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
  
       <StudentCard name="Eva" week={7} info={{ city: 'BCN', course: 'WEB' }} />
       <StudentCard name="Mat" week={8} info={{ city: 'MIA', course: 'DATA' }} />
+
+    <hr/>
+    <ReactPlayer url ="https://vimeo.com/channels/top/22439234" playing controls/>
+
     </div>
   );
 }
